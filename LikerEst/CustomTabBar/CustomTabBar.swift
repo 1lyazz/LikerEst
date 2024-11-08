bebc9c806bae65df9d32f097453b4ca1a6b4d3d8
Ilya Zablotski <zablotski2020@gmail.com>
2024-11-08 12:24:14 +0300

    Add Сustom TabBar

---
+++ LikerEst/CustomTabBar/CustomTabBar.swift
@@ -0,0 +1,94 @@
+//
+//  CustomTabBar.swift
+//  LikerEst
+//
+//  Created by Ilya Zablotski on 8.11.24.
+//
+
+import RxCocoa
+import RxGesture
+import RxSwift
+import UIKit
+
+final class CustomTabBar: UIStackView {
+    var itemTapped: Observable<Int> { itemTappedSubject.asObservable() }
+    
+    private let homeItem = CustomItemView(with: .home, index: 0)
+    private let favouritesItem = CustomItemView(with: .favourites, index: 1)
+    
+    private lazy var customItemViews: [CustomItemView] = [homeItem, favouritesItem]
+    
+    private let itemTappedSubject = PublishSubject<Int>()
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - Init
+    
+    init() {
+        super.init(frame: .zero)
+        
+        setupView()
+        setupProperties()
+        bind()
+        
+        setNeedsLayout()
+        layoutIfNeeded()
+        selectItem(index: 0)
+    }
+    
+    @available(*, unavailable)
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup View
+    
+    private func setupView() {
+        addArrangedSubviews([homeItem, favouritesItem])
+    }
+    
+    // MARK: - Setup Properties
+    
+    private func setupProperties() {
+        distribution = .fillEqually
+        alignment = .center
+                
+        setupCornerRadius(30)
+        
+        for customItemView in customItemViews {
+            customItemView.translatesAutoresizingMaskIntoConstraints = false
+            customItemView.clipsToBounds = true
+        }
+    }
+    
+    // MARK: - Bindings
+    
+    private func bind() {
+        homeItem.rx.tapGesture()
+            .when(.recognized)
+            .bind { [weak self] _ in
+                guard let self = self else { return }
+                self.homeItem.animateClick {
+                    self.selectItem(index: self.homeItem.index)
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        favouritesItem.rx.tapGesture()
+            .when(.recognized)
+            .bind { [weak self] _ in
+                guard let self = self else { return }
+                self.favouritesItem.animateClick {
+                    self.selectItem(index: self.favouritesItem.index)
+                }
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    // MARK: - Methods
+    
+    private func selectItem(index: Int) {
+        customItemViews.forEach { $0.isSelected = $0.index == index }
+        itemTappedSubject.onNext(index)
+    }
+}
+
---
+++ LikerEst/CustomTabBar/TabBarController.swift
@@ -0,0 +1,93 @@
+//
+//  TabBarController.swift
+//  LikerEst
+//
+//  Created by Ilya Zablotski on 8.11.24.
+//
+
+import RxSwift
+import UIKit
+
+final class TabBarController: UITabBarController {
+    private lazy var customTabBar: CustomTabBar = {
+        let tabBar = CustomTabBar()
+        tabBar.translatesAutoresizingMaskIntoConstraints = false
+        tabBar.backgroundColor = .black
+        tabBar.alpha = 0.8
+        tabBar.addShadow()
+        return tabBar
+    }()
+    
+    private let disposeBag = DisposeBag()
+
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        makeConstraints()
+        setupProperties()
+        bind()
+        view.layoutIfNeeded()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    // MARK: - Setup View
+    
+    private func setupView() {
+        view.addSubview(customTabBar)
+    }
+    
+    // MARK: - Constraints
+        
+    private func makeConstraints() {
+        customTabBar.anchor(
+            left: view.safeAreaLayoutGuide.leftAnchor,
+            bottom: view.safeAreaLayoutGuide.bottomAnchor,
+            right: view.safeAreaLayoutGuide.rightAnchor,
+            paddingLeft: 24,
+            paddingRight: 24,
+            height: 80
+        )
+    }
+    
+    // MARK: - Setup Properties
+    
+    private func setupProperties() {
+        tabBar.isHidden = true
+        selectedIndex = 0
+        let controllers = CustomTabItem.allCases.map { $0.viewController }
+        setViewControllers(controllers, animated: true)
+        setupTabBarAppearance()
+    }
+    
+    // MARK: - Bindings
+    
+    private func bind() {
+        customTabBar.itemTapped
+            .bind { [weak self] in self?.selectTabWith(index: $0) }
+            .disposed(by: disposeBag)
+    }
+    
+    // MARK: - Methods
+    
+    private func selectTabWith(index: Int) {
+        selectedIndex = index
+    }
+    
+    private func setupTabBarAppearance() {
+        guard #available(iOS 13.0, *) else { return }
+        
+        let appearance = UITabBarAppearance()
+        appearance.configureWithTransparentBackground()
+        tabBar.standardAppearance = appearance
+        
+        if #available(iOS 15.0, *) {
+            tabBar.scrollEdgeAppearance = appearance
+        }
+    }
+}
