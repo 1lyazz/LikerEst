8d5b4f06e70ff19217412040469790ade67e00a3
Ilya Zablotski <zablotski2020@gmail.com>
2024-11-10 22:23:30 +0300

    Add & Setup DataModel: LikeEst Data Model CoreDataManager

---
+++ LikerEst/Services/CoreDataManager.swift
@@ -0,0 +1,40 @@
+//
+//  CoreDataManager.swift
+//  LikerEst
+//
+//  Created by Ilya Zablotski on 10.11.24.
+//
+
+import CoreData
+
+class CoreDataManager {
+    static let shared = CoreDataManager()
+
+    // NSPersistentContainer to manage Core Data stack
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "LikerEst")
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+
+    var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+
+    // Saves the changes in the Core Data context
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
