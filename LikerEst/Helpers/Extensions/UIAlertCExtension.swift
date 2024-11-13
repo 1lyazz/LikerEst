7b51bc928af3cea6969592272671011de3e43643
Ilya Zablotski <zablotski2020@gmail.com>
2024-11-13 17:30:02 +0300

    Fix Download Alert: If there are no permissions to display an alert

--- LikerEst/Helpers/Extensions/UIAlertCExtension.swift
+++ LikerEst/Helpers/Extensions/UIAlertCExtension.swift
@@ -15,13 +15,30 @@ extension UIAlertController {
         Downloads: \(photo.downloads ?? 0)
         Description: \(photo.altDescription ?? "meow")
         """
+        
         let alert = UIAlertController(title: "Details", message: message, preferredStyle: .alert)
-
         let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
         okAction.setValue(UIColor.label, forKey: "titleTextColor")
-
+        
         alert.addAction(okAction)
-
         return alert
+    }
+
+    static func showAlertForPhotoAccess(presentingViewController: UIViewController) {
+        let alert = UIAlertController(title: "Oops!", message: "To save photos, you'll need to allow Photos access in Settings", preferredStyle: .alert)
+        let settingsAction = UIAlertAction(title: "Open Settings", style: .default) { _ in
+            if let url = URL(string: UIApplication.openSettingsURLString), UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+        settingsAction.setValue(UIColor.label, forKey: "titleTextColor")
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        cancelAction.setValue(UIColor.label, forKey: "titleTextColor")
+        
+        alert.addAction(settingsAction)
+        alert.addAction(cancelAction)
+        
+        presentingViewController.present(alert, animated: true, completion: nil)
     }
 }
